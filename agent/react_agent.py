d3f01e69d6bc16628ddc046ec87ece614e0bd6da
fengwx <fengwx@damddos.com>
2026-03-02 14:17:54 +0800

    1

--- agent/react_agent.py
+++ agent/react_agent.py
@@ -1,5 +1,7 @@
 from langchain.agents import create_agent
 
+from agent.tools.agent_tools import link_database
+from agent.tools.middleware import monitor_tool, log_before_model
 from model.factory import chat_model
 from utils.prompt_loader import load_system_prompts
 
@@ -9,8 +11,8 @@ class ReactAgent:
         self.agent = create_agent(
             model=chat_model,
             system_prompt=load_system_prompts(),
-            tools=[],
-            middleware=[],
+            tools=[link_database],
+            middleware=[monitor_tool, log_before_model],
         )
 
     def execute_stream(self, query: str):
@@ -29,6 +31,6 @@ class ReactAgent:
 if __name__ == '__main__':
     agent = ReactAgent()
 
-    query = "你好，请介绍一下自己"
+    query = "数据库中有哪些表?"
     for chunk in agent.execute_stream(query):
         print(chunk, end="", flush=True)
---
+++ agent/tools/agent_tools.py
@@ -0,0 +1,9 @@
+
+
+from langchain_core.tools import tool
+
+@tool(description="连接数据库的示例，查询数据库中数据")
+def link_database_demo(query: str) -> str:
+    return "数据库连接成功，数据库中共有10张表，分别是：表1，表2，表3，表4，表5，表6，表7，表8，表9，表10"
+
+
